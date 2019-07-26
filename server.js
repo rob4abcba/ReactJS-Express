383fa183b12b18901c7045d8f0a8481ee6d6157b
rob4abcba <rob_abcba@yahoo.com>
2019-07-25 18:08:53 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// Bring in Express
+const express = require('express');
+
+// Initialize Express
+const app = express();
+
+// Add just one route to return an array of customers
+// Use route = /api/customers
+// req = request ;  res = response ;
+// We create some dummy hard-coded customer data, but in real life this will pull from a database.
+// res.json(customers) = respond with json and pass in customers
+app.get('/api/customers', (req, res) => {
+    const customers = [
+        {id: 1, firstName: 'John', lastName: 'Doe'},
+        {id: 2, firstName: 'Steve', lastName: 'Smith'},
+        {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+    ];
+    res.json(customers);
+});
+
+// Create a port variable to listen on, but do NOT choose 3000 since 3000 is the default used by ReactJS create-react-app.
+const port = 5001;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
